5d003cabca54232c1e71e27f17253bd211a6996a
Byron2016 <bgva2005@yahoo.com>
2024-05-30 15:58:25 -0500

    feat: add to class library language some classes

---
+++ 079_CSharp_PerfectConsoleAppSol/HelloWorldLibrary/BusinessLogic/Messages.cs
@@ -0,0 +1,63 @@
+﻿using HelloWorldLibrary.Models;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace HelloWorldLibrary.BusinessLogic;
+
+public class Messages : IMessages
+{
+    private readonly ILogger<Messages> _log;
+
+    public Messages(ILogger<Messages> log)
+    {
+        _log = log;
+    }
+
+    public string Greeting(string language)
+    {
+        string output = LookUpCustomText("Greeting", language);
+        //string output = LookUpCustomText(nameof(Greeting), language);
+        return output;
+    }
+
+    private string LookUpCustomText(string key, string language)
+    {
+        JsonSerializerOptions options = new JsonSerializerOptions()
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+
+        /*
+        // También se podía poner así:
+        JsonSerializerOptions options = new()
+        {
+            PropertyNameCaseInsensitive = true,
+        };
+        */
+
+        try
+        {
+            List<CustomText>? messageSets = JsonSerializer
+            .Deserialize<List<CustomText>>
+            (
+                File.ReadAllText("CustomText.json"), options
+            );
+
+            CustomText? messages = messageSets?.Where(x => x.Languge == language).First();
+
+            if (messages is null)
+            {
+                throw new NullReferenceException("The specified language was not found in json file");
+            }
+
+            return messages.Transltations[key];
+        }
+        catch (Exception ex)
+        {
+            _log.LogError("Error lookin up the custom text", ex);
+            throw;
+        }
+    }
+
+
+}
---
+++ 079_CSharp_PerfectConsoleAppSol/HelloWorldLibrary/Models/CustomText.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloWorldLibrary.Models;
+
+public class CustomText
+{
+    public string Languge { get; set; }
+    public Dictionary<string, string> Transltations { get; set; }
+}
